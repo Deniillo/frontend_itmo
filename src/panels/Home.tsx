577cadab2cce0cb3b36ff7046d943f2e81d20aab
deniill <aleeenin,daniil@yandex.ru>
2024-09-14 01:23:43 +0300

    Initial commit

---
+++ src/panels/Home.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react';
+import {
+  Panel,
+  PanelHeader,
+  Header,
+  Button,
+  Group,
+  Cell,
+  Div,
+  Avatar,
+  NavIdProps,
+} from '@vkontakte/vkui';
+import { UserInfo } from '@vkontakte/vk-bridge';
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
+import bridge from '@vkontakte/vk-bridge';
+
+export interface HomeProps extends NavIdProps {
+  fetchedUser?: UserInfo;
+}
+
+export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
+  const { photo_200, city, first_name, last_name } = { ...fetchedUser };
+  const routeNavigator = useRouteNavigator();
+
+  async function getHistory() {
+    const urlImage = (await (await fetch("https://randomfox.ca/floof/")).json()).image;
+
+    bridge.send('VKWebAppShowStoryBox', {
+      background_type: 'image',
+      url : urlImage
+    });
+  }
+
+  return (
+    <Panel id={id}>
+      <PanelHeader>Главная</PanelHeader>
+      {fetchedUser && (
+        <Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+          <Cell before={photo_200 && <Avatar src={photo_200} />} subtitle={city?.title}>
+            {`${first_name} ${last_name}`}
+          </Cell>
+        </Group>
+      )}
+
+      <Group header={<Header mode="secondary">Navigation Example</Header>}>
+        <Div>
+          <Button stretched size="l" mode="secondary" onClick={getHistory}>
+            Очень смешная кнопка
+          </Button>
+        </Div>
+      </Group>
+    </Panel>
+  );
+};
